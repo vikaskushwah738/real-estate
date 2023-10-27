fc27f1feabcd4c416348b2d206ebf9fc7a1fc1d0
vikaskushwah738 <vikaskushwah738@gmail.com>
2023-10-28 00:18:40 +0530

    created new files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Singin from './pages/Singin';
+import Singout from './pages/Singout';
+import Singup from './pages/Singup';
+import Profile from './pages/Profile';
+
 
 export default function App() {
-  return (
-    <div className='text-red-500'>App</div>
-  )
+  return <BrowserRouter>
+  <Routes>
+  <Route path="/" element ={<Home />}/>
+  <Route path="/about" element ={<About />}/>
+  <Route path="/sing-in" element ={<Singin />}/>
+  <Route path="/sing-out" element ={<Singout />}/>
+  <Route path="/sing-up" element ={<Singup />}/>
+  <Route path="/profile" element ={<Profile />}/>
+
+  </Routes>
+  </BrowserRouter>
 }
